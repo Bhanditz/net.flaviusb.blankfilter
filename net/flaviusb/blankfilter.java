2116d39254827a311b125240db7a97cb906576a2
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2016-08-17 13:04:53 +1200

    Try copying the model through the modelhelper.

--- net/flaviusb/blankfilter.java
+++ net/flaviusb/blankfilter.java
@@ -3,9 +3,19 @@ package net.flaviusb;
 import org.bimserver.emf.IfcModelInterface;
 import org.bimserver.plugins.ModelHelper;
 import org.bimserver.plugins.Reporter;
+import org.bimserver.emf.IdEObject;
+import org.bimserver.emf.IfcModelInterfaceException;
 
 public class blankfilter implements nz.ac.auckland.cs.QueryInterface {
   @Override public IfcModelInterface query(IfcModelInterface model, Reporter reporter, ModelHelper modelHelper) {
-    return model;
+    try {
+      for (IdEObject idEObject : model.getValues()) {
+        modelHelper.copy(idEObject, false);
+      }
+      return modelHelper.getTargetModel();
+    } catch (IfcModelInterfaceException e) {
+      reporter.error(e);
+    }
+    return null;
   }
 }
